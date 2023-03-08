f21d4c12fdc8b15c16672056b6f75327ce6cc524
James Oladimeji <elderjames314@gmail.com>
2023-03-08 08:16:47 +0100

    first commit

---
+++ src/main/java/com/klash/deliverydispatcher/DeliveryDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.klash.deliverydispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryDispatcherApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/klash/deliverydispatcher/user/Staff.java
@@ -0,0 +1,8 @@
+package com.klash.deliverydispatcher.user;
+
+public class Staff {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
